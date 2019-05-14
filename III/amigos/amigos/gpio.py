dc8a0eae844b5eb1d346be5766ef903922161809
coovi meha <coovi.meha@colorado.edu>
2019-05-14 16:56:40 -0600

    * Finished control of GPS and router power on and off, finish watchdog and add 3 min and 1 hour of board to go to sheep

--- III/amigos/amigos/gpio.py
+++ III/amigos/amigos/gpio.py
@@ -5,7 +5,6 @@ from time import sleep
 def toggle(bit):
 
     subprocess.call("echo {0} > /sys/class/gpio/pwr_ctl/index".format(bit), shell=True)
-    print bit
 
 
 def router_on(bit):
@@ -13,7 +12,6 @@ def router_on(bit):
         toggle(bit)
         subprocess.call("echo 0x8 > /sys/class/gpio/pwr_ctl/data", shell=True)
         print "router is turned on"
-        print bit
 
 
 def router_off(bit):
@@ -22,7 +20,6 @@ def router_off(bit):
         sleep(1)
         subprocess.call("echo 0xF6 > /sys/class/gpio/pwr_ctl/data", shell=True)
         print "router is turned off"
-        print bit
 
 
 def gps_on(bit):
@@ -30,7 +27,6 @@ def gps_on(bit):
         toggle(bit-1)
         subprocess.call("echo 0x02 > /sys/class/gpio/pwr_ctl/data", shell=True)
         print "gps module is turned on"
-        print bit
 
 
 def gps_off(bit):
@@ -38,4 +34,3 @@ def gps_off(bit):
         toggle(bit-1)
         subprocess.call("echo 0xFC > /sys/class/gpio/pwr_ctl/data", shell=True)
         print "gps module is turned off"
-        print bit
