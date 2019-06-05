9a862fb1b192ce24192b29b65aaae9ccdef8fc6e
coovi meha <coovi.meha@colorado.edu>
2019-06-05 17:43:30 -0600

    update cammera code by adding pan and titl functionality

--- III/amigos/cli.py
+++ III/amigos/cli.py
@@ -4,6 +4,7 @@ import os.path
 import amigos.argparse as argparse
 import amigos.watchdog as watchdog
 import amigos.gpio as gpio
+
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "text.txt")
 
@@ -17,13 +18,15 @@ def main():
     parser = argparse.ArgumentParser(prog='Amigos', add_help=False)
     # Group or command for schedule viewing
     schedule = parser.add_argument_group(
-        'Set or View Schedules', 'Show all the schedules')
+        'Set or View Schedules', 'Show all the schedules'
+    )
+    schedule.add_argument('schedule', help='View all pending schedule', nargs='?')
     schedule.add_argument(
-        'schedule', help='View all pending schedule', nargs='?')
+        '-s', '--summer', help='View summer schedule', action='store_true'
+    )
     schedule.add_argument(
-        '-s', '--summer', help='View summer schedule', action='store_true')
-    schedule.add_argument(
-        '-w', '--winter', help='View winter schedule', action='store_true')
+        '-w', '--winter', help='View winter schedule', action='store_true'
+    )
 
     # group of command for weather viewing
     weather = parser.add_argument_group('Read weather', 'show weather data')
@@ -33,43 +36,57 @@ def main():
     # group of command for watchdog configureting
     wdog = parser.add_argument_group('Set Watchdog', 'Change watch dog setup')
     wdog.add_argument('watchdog', help='View running watchdog setting', nargs='?')
-    wdog.add_argument('-u', '--update',
-                      help='update the watchdog cycle', action='store_true')
-    wdog.add_argument('-sl', '--sleep',
-                      help='Put board to sleep', action='store_true')
-    wdog.add_argument('-d', '--deactivate',
-                      help='deactivate watchdog from auto update', action='store_true')
+    wdog.add_argument(
+        '-u', '--update', help='update the watchdog cycle', action='store_true'
+    )
+    wdog.add_argument('-sl', '--sleep', help='Put board to sleep', action='store_true')
+    wdog.add_argument(
+        '-d',
+        '--deactivate',
+        help='deactivate watchdog from auto update',
+        action='store_true',
+    )
 
     # power commands
     power = parser.add_argument_group('Power Control', 'Control power on gpio pins')
     power.add_argument(
-        'power', help='Need one of the secondary arguments bellow', nargs='?')
-    power.add_argument('-r_on', '--router_on',
-                       help='Router on', action='store_true')
-    power.add_argument('-r_off', '--router_off',
-                       help='Router off', action='store_true')
-    power.add_argument('-g_on', '--gps_on',
-                       help='GPS on', action='store_true')
-    power.add_argument('-g_off', '--gps_off',
-                       help='GPS off', action='store_true')
-    power.add_argument('-w_on', '--weather_on',
-                       help='Weather station on', action='store_true')
-    power.add_argument('-w_off', '--weather_off',
-                       help='Weather station off', action='store_true')
-    power.add_argument('-down', '--power_down',
-                       help='power down all peripherals', action='store_true')
-    power.add_argument('-up', '--power_up',
-                       help='power up all peripherals', action='store_true')
+        'power', help='Need one of the secondary arguments bellow', nargs='?'
+    )
+    power.add_argument('-r_on', '--router_on', help='Router on', action='store_true')
+    power.add_argument('-r_off', '--router_off', help='Router off', action='store_true')
+    power.add_argument('-g_on', '--gps_on', help='GPS on', action='store_true')
+    power.add_argument('-g_off', '--gps_off', help='GPS off', action='store_true')
+    power.add_argument(
+        '-w_on', '--weather_on', help='Weather station on', action='store_true'
+    )
+    power.add_argument(
+        '-w_off', '--weather_off', help='Weather station off', action='store_true'
+    )
+    power.add_argument(
+        '-off', '--power_off', help='power down all peripherals', action='store_true'
+    )
+    power.add_argument(
+        '-on', '--power_on', help='power up all peripherals', action='store_true'
+    )
 
     camera = parser.add_argument_group(
-        'Control Camera', 'Control camera position, take pictures and more')
+        'Control Camera', 'Control camera position, take pictures and more'
+    )
+    camera.add_argument('camera', help='required a secondary command', nargs='?')
+    camera.add_argument('-up', '--move_up', help='Move camera up', action='store_true')
     camera.add_argument(
-        'camera', help='required a secondary command', nargs='?')
+        '-up', '--move_up', help='Move camera down', action='store_true'
+    )
+    camera.add_argument(
+        '-left', '--move_left', help='Move camera to the left', action='store_true'
+    )
+    camera.add_argument(
+        '-right', '--move_right', help='Move camera to the right', action='store_true'
+    )
 
     # help command
     h = parser.add_argument_group('Help', 'show help menu')
-    h.add_argument('-h', '--help',
-                   help='Show this menu', action='store_true')
+    h.add_argument('-h', '--help', help='Show this menu', action='store_true')
 
     # retrieve all arguments entered
     args = parser.parse_args()
@@ -81,12 +98,16 @@ def main():
     elif args.schedule == 'watchdog':
         if args.update:
             watchdog.set_mode(
-                mode=int(input("Enter 1 for an hour and 0 for 3 minutes watchdog reset:\n")))
+                mode=int(
+                    input("Enter 1 for an hour and 0 for 3 minutes watchdog reset:\n")
+                )
+            )
         elif args.deactivate:
             watchdog.set_mode(default=True)
         elif args.sleep:
             watchdog.set_mode(
-                mode=int(input("Enter 2 for an hour and 3 for 3 minutes of sleep:\n")))
+                mode=int(input("Enter 2 for an hour and 3 for 3 minutes of sleep:\n"))
+            )
         else:
             watchdog.set_mode(mode=None)
     elif args.schedule == 'power':
