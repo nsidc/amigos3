b5fb0915ef50ed36f988349ed69185865ce3b804
coovi meha <coovi.meha@colorado.edu>
2019-06-12 09:19:13 -0600

    saparate files

--- I/set_summer_mode.sh
+++
@@ -1,7 +0,0 @@
-mount / -o remount,rw
-cp /mnt/S82ron.sh.in_A2_firnaqua_summer /etc/rc2.d/S82ron.sh
-chmod +x /etc/rc2.d/S82ron.sh
-mount / -o remount,ro
-
-cp /mnt/sked/schedule.dat.summer /mnt/sked/schedule.dat
-cp /mnt/sked/schedule.dat.summer /var/schedule.dat
