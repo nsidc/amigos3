b83ef55759e2438cded3940968dbe4d70cca574b
coovi meha <coovi.meha@colorado.edu>
2019-06-12 09:19:13 -0600

    saparate files

--- I/sked_tasks/upload_chunks_tps.sh
+++
@@ -1,5 +0,0 @@
-# Cronjob has no HOME, so wont find e.g. .netrc file
-HOME=/
-export HOME
-
-/mnt/sked_tasks/upload_chunks.sh /mnt/grs/tpsfilelist > /mnt/logs/upload_chunks_tps.log
--- I/sony/capture.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget http://192.168.0.20:8082/oneshotimage.jpg -O image.jpg
-
