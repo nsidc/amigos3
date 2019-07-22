18c0808b98bb13fcedb881a021cacc3605b0de94
Bruce Wallin <bruce.wallin@nsidc.org>
2019-07-22 15:59:53 -0600

    Move submodule import shim to amigos (sub)-package

--- amigos/amigos.sh
+++ amigos/amigos.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-AMIGOS_DIR=/media/mmcblk0p1
+AMIGOS_DIR=/media/mmcblk0/amigos
 if [[ "$1" == "watchdog" ]]
 then
-    cd $AMIGOS_DIR && python -m amigos.cli $1 $2 &
+    cd $AMIGOS_DIR && python cli.py $1 $2 &
 else
-    cd $AMIGOS_DIR && python -m amigos.cli $1 $2 $3
+    cd $AMIGOS_DIR && python cli.py $1 $2 $3
 fi
