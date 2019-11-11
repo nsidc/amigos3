e44e53aad08909bbf92d6b4c7d35f068acb133b1
Bruce Wallin <bruce.wallin@nsidc.org>
2019-11-11 16:16:12 -0700

    Reorganize unit tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import pty
 import threading
---
+++ test/test_schedule.py
@@ -0,0 +1,8 @@
+import pytest
+
+from honcho.core.scheduler import select_schedule
+
+
+@pytest.mark.skip()
+def test_select_schedule():
+    pass
