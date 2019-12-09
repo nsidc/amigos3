35285c11d7c3b7a7a6f4c24b7b4eeb32ab7f21e3
Bruce Wallin <bruce.wallin@nsidc.org>
2019-12-09 02:55:37 -0700

    Add watchdog tick functions

--- honcho/core/system.py
+++ honcho/core/system.py
@@ -2,6 +2,7 @@ import subprocess
 import logging
 
 from honcho.core.gpio import all_off
+from honcho.config import WATCHDOG_DEVICE
 
 logger = logging.getLogger(__name__)
 
@@ -18,3 +19,24 @@ def reboot():
     all_off()
     logger.info('Rebooting system')
     subprocess.call("reboot", shell=True)
+
+
+def sleep(td):
+    all_off()
+    subprocess.check_call(['apmsleep', '+{0}:{1}'.format(td.hours, td.seconds)])
+
+
+def watchdog_tick_3min():
+    with open(WATCHDOG_DEVICE, 'wb') as f:
+        f.write(hex(1))
+        f.write(hex(0))
+        f.write(hex(1))
+    logger.info('Watchdog ticked for 3 minutes')
+
+
+def watchdog_tick_1hour():
+    with open(WATCHDOG_DEVICE, 'wb') as f:
+        f.write(hex(3))
+        f.write(hex(0))
+        f.write(hex(3))
+    logger.info('Watchdog ticked for 1 hour')
