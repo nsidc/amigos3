77ce63419271804d983d7644ec69baeba65c0876
Bruce Wallin <bruce.wallin@nsidc.org>
2019-12-24 19:16:40 -0700

    Move schedule sleep into voltage check loop
    Reduces logging runaway when STAY_AWAKE set

--- honcho/core/sched.py
+++ honcho/core/sched.py
@@ -85,12 +85,13 @@ def execute():
     load_schedule(scheduler, SCHEDULES[name])
 
     while True:
+        sleep(SCHEDULE_IDLE_CHECK_INTERVAL)
         if not voltage_check():
+            # System just wokeup, so skip schedule check and do another voltage check
             continue
 
         scheduler.run_pending()
         idle_check(scheduler)
-        sleep(SCHEDULE_IDLE_CHECK_INTERVAL)
 
 
 if __name__ == '__main__':
