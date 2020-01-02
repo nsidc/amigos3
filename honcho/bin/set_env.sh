2dc617b76cf3613b2b3c4ec4b6439d3c33911699
Bruce Wallin <bruce.wallin@nsidc.org>
2020-01-02 01:42:42 -0700

    Set normal operating mode for imminent deployment

--- honcho/bin/set_env.sh
+++ honcho/bin/set_env.sh
@@ -2,8 +2,8 @@
 source /media/mmcblk0p1/honcho/bin/set_paths.sh
 
 export LOG_LEVEL=DEBUG
-export MODE=TEST
-export KEEP_AWAKE=1
-export HUB_ALWAYS_ON=1
+export MODE=NORMAL
+export KEEP_AWAKE=0
+export HUB_ALWAYS_ON=0
 export SKIP_MAINTENANCE=0
 export IGNORE_LOW_VOLTAGE=0
