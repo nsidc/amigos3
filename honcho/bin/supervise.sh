afb87b9b3f0b3cc65276e7523bafa1ded632e484
Bruce Wallin <bruce.wallin@nsidc.org>
2021-07-30 09:42:04 -0600

    Make changes to scripts/env for testing

--- honcho/bin/supervise.sh
+++ honcho/bin/supervise.sh
@@ -2,4 +2,5 @@
 source /media/mmcblk0p1/honcho/bin/set_env.sh
 
 mkdir -p /media/mmcblk0p1/logs
-honcho supervise --run >> /media/mmcblk0p1/logs/supervisor.log 2>&1
+# Commented so that honcho schedule does not run on startup/cron etc... during testing
+# honcho supervise --run >> /media/mmcblk0p1/logs/supervisor.log 2>&1
